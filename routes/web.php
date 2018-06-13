6b96f0a854c297e972563b666a990baa58b0d491
shigo.kin <kinsigou@hotmail.com>
2018-06-13 12:29:34 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@
 //    return view('welcome');
 //});
 Route::get('/','WelcomeController@index');
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
