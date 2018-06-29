0138bd7ef2d5018c0598a897dc16e72764a958fc
shigo.kin <kinsigou@hotmail.com>
2018-06-29 03:57:33 +0000

    welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <div class="cover-inner">
             <div class="cover-contents">
                 <h1>素敵なモノと出会う場所</h1>
+                <h1>門リストブラン地　</h1>
                 @if (!Auth::check())
                     <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
                 @endif
@@ -16,5 +17,14 @@
 @section('content')
     @include('items.items')
     {!! $items->render() !!}
+<<<<<<<<< saved version
     
+=========
+<<<<<<<<< saved version
+
+=========
+    {!! $items->render() !!}
+    
+>>>>>>>>> local version
+>>>>>>>>> local version
 @endsection
